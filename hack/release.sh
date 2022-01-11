e20ec139603c2247440c9d18bda286a11f7e856b
Revital Sur <eres@il.ibm.com>
2022-01-11 06:33:11 +0200

    Update release related files towards 0.5.0 release.
    Signed-off-by: Revital Sur <eres@il.ibm.com>

--- hack/release.sh
+++ hack/release.sh
@@ -14,3 +14,6 @@ ${TOOLBIN}/yq eval --inplace ".version = \"$RELEASE\"" ./modules/fybrik-implicit
 
 ${TOOLBIN}/yq eval --inplace ".spec.chart.name = \"ghcr.io/fybrik/fybrik-implicit-copy-batch:$RELEASE\"" ./modules/implicit-copy-batch-module.yaml
 ${TOOLBIN}/yq eval --inplace ".spec.chart.name = \"ghcr.io/fybrik/fybrik-implicit-copy-stream:$RELEASE\"" ./modules/implicit-copy-stream-module.yaml
+
+${TOOLBIN}/yq eval --inplace ".metadata.labels.version = \"$RELEASE\"" ./modules/implicit-copy-batch-module.yaml
+${TOOLBIN}/yq eval --inplace ".metadata.labels.version = \"$RELEASE\"" ./modules/implicit-copy-stream-module.yaml
