b9ec8b086e56708c8dd3822f2db06ba98306f0a7
nassarmu10 <mohammad.nassar@ibm.com>
2022-01-29 12:20:20 +0200

    Add an output check to the test of the module.
    Signed-off-by: nassarmu10 <mohammad.nassar@ibm.com>

--- hack/tools/test_module.sh
+++ hack/tools/test_module.sh
@@ -15,8 +15,8 @@ module=$4
 
 if [ $kubernetesVersion == "kind19" ]
 then
-    kind delete cluster
-    kind create cluster --image=kindest/node:v1.19.11@sha256:07db187ae84b4b7de440a73886f008cf903fcf5764ba8106a9fd5243d6f32729
+    bin/kind delete cluster
+    bin/kind create cluster --image=kindest/node:v1.19.11@sha256:07db187ae84b4b7de440a73886f008cf903fcf5764ba8106a9fd5243d6f32729
 
 elif [ $kubernetesVersion == "kind21" ]
 then
@@ -34,13 +34,13 @@ fi
 
 #quick start
 
-helm repo add jetstack https://charts.jetstack.io
-helm repo add hashicorp https://helm.releases.hashicorp.com
-helm repo add fybrik-charts https://fybrik.github.io/charts
-helm repo update
+bin/helm repo add jetstack https://charts.jetstack.io
+bin/helm repo add hashicorp https://helm.releases.hashicorp.com
+bin/helm repo add fybrik-charts https://fybrik.github.io/charts
+bin/helm repo update
 
 
-helm install cert-manager jetstack/cert-manager \
+bin/helm install cert-manager jetstack/cert-manager \
     --namespace cert-manager \
     --version v1.2.0 \
     --create-namespace \
@@ -49,14 +49,14 @@ helm install cert-manager jetstack/cert-manager \
 
 
 
-helm install vault fybrik-charts/vault --create-namespace -n fybrik-system \
+bin/helm install vault fybrik-charts/vault --create-namespace -n fybrik-system \
         --set "vault.injector.enabled=false" \
         --set "vault.server.dev.enabled=true" \
         --values https://raw.githubusercontent.com/fybrik/fybrik/v0.5.3/charts/vault/env/dev/vault-single-cluster-values.yaml
-    kubectl wait --for=condition=ready --all pod -n fybrik-system --timeout=400s
+    bin/kubectl wait --for=condition=ready --all pod -n fybrik-system --timeout=400s
 
-helm install fybrik-crd fybrik-charts/fybrik-crd -n fybrik-system --version v$fybrikVersion --wait
-helm install fybrik fybrik-charts/fybrik -n fybrik-system --version v$fybrikVersion --wait
+bin/helm install fybrik-crd fybrik-charts/fybrik-crd -n fybrik-system --version v$fybrikVersion --wait
+bin/helm install fybrik fybrik-charts/fybrik -n fybrik-system --version v$fybrikVersion --wait
 
 
 # cd /data/checkagain14/data-movement-operator/
@@ -65,7 +65,7 @@ then
     git clone https://github.com/fybrik/data-movement-operator.git
     cd data-movement-operator/
     git checkout releases/0.6.0
-    helm install data-movement-operator charts/data-movement-operator -n fybrik-system --wait
+    bin/helm install data-movement-operator charts/data-movement-operator -n fybrik-system --wait
     cd ..
     rm -rf data-movement-operator
 fi
@@ -79,13 +79,13 @@ fi
 #kubectl apply -f https://github.com/fybrik/arrow-flight-module/releases/latest/download/module.yaml -n fybrik-system
 # kubectl apply -f ${WORKING_DIR}/arrow_$moduleVersion_noactions.yaml -n fybrik-system
 
-kubectl apply -f https://github.com/fybrik/arrow-flight-module/releases/download/v$moduleVersion/module.yaml -n fybrik-system
+bin/kubectl apply -f https://github.com/fybrik/arrow-flight-module/releases/download/v$moduleVersion/module.yaml -n fybrik-system
 #kubectl apply -f ${WORKING_DIR}/flight.yaml -n fybrik-system
 if [ $module == "stream" ]
 then
-    kubectl apply -f https://github.com/fybrik/data-movement-operator/releases/download/v$moduleVersion/implicit-copy-stream-module.yaml -n fybrik-system
+    bin/kubectl apply -f https://github.com/fybrik/data-movement-operator/releases/download/v$moduleVersion/implicit-copy-stream-module.yaml -n fybrik-system
 else
-    kubectl apply -f https://github.com/fybrik/data-movement-operator/releases/download/v$moduleVersion/implicit-copy-batch-module.yaml -n fybrik-system
+    bin/kubectl apply -f https://github.com/fybrik/data-movement-operator/releases/download/v$moduleVersion/implicit-copy-batch-module.yaml -n fybrik-system
 fi
 # kubectl apply -f /data/checkagain14/data-movement-operator/modules/implicit-copy-batch-module.yaml -n fybrik-system
 
@@ -98,8 +98,8 @@ fi
 
 
 #datashim
-kubectl apply -f https://raw.githubusercontent.com/datashim-io/datashim/master/release-tools/manifests/dlf.yaml
-kubectl wait --for=condition=ready pods -l app.kubernetes.io/name=dlf -n dlf --timeout=500s
+bin/kubectl apply -f https://raw.githubusercontent.com/datashim-io/datashim/master/release-tools/manifests/dlf.yaml
+bin/kubectl wait --for=condition=ready pods -l app.kubernetes.io/name=dlf -n dlf --timeout=500s
 # cd $PATH_TO_LOCAL_FYBRIK/third_party/datashim/
 # make deploy
 
@@ -111,15 +111,15 @@ kubectl wait --for=condition=ready pods -l app.kubernetes.io/name=dlf -n dlf --t
 
 # Notebook sample
 
-kubectl create namespace fybrik-notebook-sample
-kubectl config set-context --current --namespace=fybrik-notebook-sample
+bin/kubectl create namespace fybrik-notebook-sample
+bin/kubectl config set-context --current --namespace=fybrik-notebook-sample
 
 #localstack
-helm repo add localstack-charts https://localstack.github.io/helm-charts
-helm install localstack localstack-charts/localstack --set startServices="s3" --set service.type=ClusterIP
-kubectl wait --for=condition=ready --all pod -n fybrik-notebook-sample --timeout=400s
+bin/helm repo add localstack-charts https://localstack.github.io/helm-charts
+bin/helm install localstack localstack-charts/localstack --set startServices="s3" --set service.type=ClusterIP
+bin/kubectl wait --for=condition=ready --all pod -n fybrik-notebook-sample --timeout=400s
 
-kubectl port-forward svc/localstack 4566:4566 &
+bin/kubectl port-forward svc/localstack 4566:4566 &
 
 export ENDPOINT="http://127.0.0.1:4566"
 export BUCKET="demo"
@@ -128,7 +128,7 @@ export FILEPATH="$WORKING_DIR/PS_20174392719_1491204439457_log.csv"
 aws configure set aws_access_key_id ${ACCESS_KEY} && aws configure set aws_secret_access_key ${SECRET_KEY} && aws --endpoint-url=${ENDPOINT} s3api create-bucket --bucket ${BUCKET} && aws --endpoint-url=${ENDPOINT} s3api put-object --bucket ${BUCKET} --key ${OBJECT_KEY} --body ${FILEPATH}
 
 
-cat << EOF | kubectl apply -f -
+cat << EOF | bin/kubectl apply -f -
 apiVersion: v1
 kind: Secret
 metadata:
@@ -140,13 +140,13 @@ stringData:
 EOF
 
 
-kubectl apply -f $WORKING_DIR/Asset-$moduleVersion.yaml -n fybrik-notebook-sample
+bin/kubectl apply -f $WORKING_DIR/Asset-$moduleVersion.yaml -n fybrik-notebook-sample
 
 
 
 
 #fybrikstorage
-cat << EOF | kubectl apply -f -
+cat << EOF | bin/kubectl apply -f -
 apiVersion: v1
 kind: Secret
 metadata:
@@ -160,14 +160,14 @@ stringData:
   secretAccessKey: "${SECRET_KEY}"
 EOF
 
-kubectl apply -f $WORKING_DIR/fybrikStorage-$moduleVersion.yaml -n fybrik-system
+bin/kubectl apply -f $WORKING_DIR/fybrikStorage-$moduleVersion.yaml -n fybrik-system
 
 
-kubectl -n fybrik-system create configmap sample-policy --from-file=$WORKING_DIR/sample-policy-$moduleVersion.rego
-kubectl -n fybrik-system label configmap sample-policy openpolicyagent.org/policy=rego
+bin/kubectl -n fybrik-system create configmap sample-policy --from-file=$WORKING_DIR/sample-policy-$moduleVersion.rego
+bin/kubectl -n fybrik-system label configmap sample-policy openpolicyagent.org/policy=rego
 
 c=0
-while [[ $(kubectl get cm sample-policy -n fybrik-system -o 'jsonpath={.metadata.annotations.openpolicyagent\.org/policy-status}') != '{"status":"ok"}' ]]
+while [[ $(bin/kubectl get cm sample-policy -n fybrik-system -o 'jsonpath={.metadata.annotations.openpolicyagent\.org/policy-status}') != '{"status":"ok"}' ]]
 do
     echo "waiting"
     ((c++)) && ((c==25)) && break
@@ -175,15 +175,27 @@ do
 done
 
 
-kubectl apply -f $WORKING_DIR/fybrikapplication-$moduleVersion.yaml
+bin/kubectl apply -f $WORKING_DIR/fybrikapplication-$moduleVersion.yaml
 
 c=0
-while [[ $(kubectl get fybrikapplication my-notebook -o 'jsonpath={.status.ready}') != "true" ]]
+while [[ $(bin/kubectl get fybrikapplication my-notebook -o 'jsonpath={.status.ready}') != "true" ]]
 do
     echo "waiting"
     ((c++)) && ((c==30)) && break
     sleep 6
 done
 
-kubectl get pods -n fybrik-blueprints
+bin/kubectl get pods -n fybrik-blueprints
+POD_NAME=$(bin/kubectl get pods -n fybrik-blueprints -o=name | sed "s/^.\{4\}//")
 
+
+bin/kubectl cp $WORKING_DIR/test.py ${POD_NAME}:/tmp -n fybrik-blueprints
+bin/kubectl exec -it ${POD_NAME} -n fybrik-blueprints -- python /tmp/test.py > res.out
+
+DIFF=$(diff $WORKING_DIR/expected.txt res.out)
+if [ "${DIFF}" == "" ]
+then
+    echo "test succeeded"
+else
+    echo "test failed"
+fi
